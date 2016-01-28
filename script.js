d07784062091c08cb484ea059f00f04d65e41924
i.lizunov <igor.lizunov@gmail.com>
2016-01-29 02:15:16 +0500

    implemented responsive features

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+	var isMobile = false;
+
+	if ($(window).width() < 860) { 
+		isMobile = true;
+	}
+
 	$('a.navigation').click(function(ev) {
 		ev.preventDefault();
 
@@ -7,7 +13,23 @@ $(document).ready(function() {
 		var page = $(ev.target).closest("a").data("page");
 		$(ev.target).closest("a").addClass("active");
 
+		if (isMobile) { 
+			$(".left-menu").slideUp();
+		}
+
 		$(".content > .page:visible").slideUp();
 		$(".content > #page-" + page).slideDown();
 	});
+
+	if(isMobile) {
+		$(".left-menu").hide();
+
+		$('.left-menu-toggle').click(function(ev) {
+			ev.preventDefault();
+
+			$(".left-menu").slideToggle();
+		});
+	}
+
+
 });
